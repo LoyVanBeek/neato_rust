c2fd0cf38b86f064a7782e79bce45cee5f526801
Loy van Beek <loy.vanbeek@gmail.com>
2020-09-13 11:42:51 +0200

    Parametrize which port we're using

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
+use std::{time, thread, env};
+
 use neato_driver::{DSeries, NeatoRobot, Toggle};
 use serialport::SerialPortSettings;
 
 fn main() {
-    println!("Hello robot!");
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+    let port = &args[1];
+    
+    println!("Hello robot on port {}!", port);
 
     let s = SerialPortSettings {
         baud_rate: 115200,
         ..Default::default()
     };
     
-    let comms = serialport::open_with_settings("/dev/ttyACM1", &s).unwrap();
+    println!("Opening serial port");
+    let comms = serialport::open_with_settings(port, &s).unwrap();
+    println!("Opened serial port");
+
+    println!("Creating robot");
     let mut robot = DSeries::new(comms);
+    println!("Create robot");
 
     robot.set_testmode(Toggle::On).unwrap();
-    robot.request_scan().unwrap();
-    robot.get_scan_ranges().unwrap();
+    robot.set_ldsrotation(Toggle::On).unwrap();
+    // robot.request_scan().unwrap();
+    // robot.get_scan_ranges().unwrap();
+    let ten_millis = time::Duration::from_secs(10);
+    thread::sleep(ten_millis);
+
+    robot.exit().unwrap();
 }
